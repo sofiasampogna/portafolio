17c4522c622ffa7dd6e7e82ea5e2fed4f1736890
sofiasampogna <sofiasampogna@gmail.com>
2023-05-09 20:03:28 -0600

    Github bug fix

--- personal_portafolio/src/components/SocialMedia.vue
+++ personal_portafolio/src/components/SocialMedia.vue
@@ -6,17 +6,17 @@
     <div class="nav-left">
         <div class="nav-left-item">
             <a href="https://www.linkedin.com/in/sofia-sampogna-montemayor-7b1b3b1b0/" target="_blank">
-                <img src="../assets/icons/linkedin.png" alt="linkedin" width="50" height="50">
+                <img src="../assets/icons/linkedin.png" alt="linkedin" width="40" height="40">
             </a>
         </div>
         <div class="nav-left-item">
-            <a href="www.github.com/sofiasampogna" target="_blank">
-                <img src="../assets/icons/github.png" alt="github" width="50" height="50">
+            <a href="https://www.github.com/sofiasampogna" target="_blank">
+                <img src="../assets/icons/github.png" alt="github" width="40" height="40">
             </a>
         </div>
         <div class="nav-left-item">
             <a href="https://www.instagram.com/sofiasampogna/" target="_blank">
-                <img src="../assets/icons/instagram.png" alt="instagram" width="50" height="50">
+                <img src="../assets/icons/instagram.png" alt="instagram" width="40" height="40">
             </a>
         </div>
     </div>
